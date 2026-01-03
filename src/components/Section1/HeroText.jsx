53e9936f2dd04db84fb4f889b908ed8934d6601e
Ritik Gupta <158696969+Guapt@users.noreply.github.com>
2026-01-04 00:18:13 +0530

    Add files via upload

---
+++ src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='p-6'>
+        <h3 className='mb-7 text-7xl font-bold'>Prospective<br/><span>Customer</span><br/> Segentation</h3>
+        <p className='text-xl w-[90] font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit perferendis quibusdam, at placeat hic ad consectetur voluptatem cumque? Illum dignissimos esse natus.</p>
+      </div>
+  )
+}
+
+export default HeroText
